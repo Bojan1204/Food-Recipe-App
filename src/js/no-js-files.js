2742da4bafd0de3b1ebc48fce7d2f34f901b3f66
Bojan Bosnic <56975621+Bojan1204@users.noreply.github.com>
2021-06-30 18:09:46 +0200

    first commit

---
+++ src/js/no-js-files.js
@@ -0,0 +1,10 @@
+// importing all files what is not javascript
+
+
+import '../scss/index.scss';
+import '../img/icons/favicon.ico';
+import '../img/icons/favicon.png';
+import '../../.gitkeep';
+import '../.htaccess';
+import '@fortawesome/fontawesome-free/js/all.js';
+
